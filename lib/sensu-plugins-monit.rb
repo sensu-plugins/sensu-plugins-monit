9714b5d65cae0a2b0f23df35b9952ca9f3f7f0c3
Tim Smith <tim@cozy.co>
2015-05-29 23:37:24 -0700

    Update version library

--- lib/sensu-plugins-monit.rb
+++ lib/sensu-plugins-monit.rb
@@ -1,7 +1,15 @@
+
+require 'sensu-plugins-monit/version'
+
+# Load the defaults
+
 #
-# Set gem version
+# Default class
 #
 module SensuPluginsMonit
-  # Gem version
-  VERSION = '0.0.1.alpha.1'
+  class << self
+  end
+
+  class << self
+  end
 end
