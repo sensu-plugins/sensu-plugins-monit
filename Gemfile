085ae599480faa120f3a13372e46ac56ce10565d
Matt Jones <mattjones@yieldbot.com>
2015-06-28 11:44:07 -0400

    deploy

--- Gemfile
+++ Gemfile
@@ -1,7 +1,3 @@
 source 'https://rubygems.org'
 
-<<<<<<< HEAD
-# Specify your gem's dependencies in sensu-plugins-monit.gemspec
-=======
->>>>>>> update repo
 gemspec
--- lib/sensu-plugins-monit.rb
+++ lib/sensu-plugins-monit.rb
@@ -1,15 +1,1 @@
-
 require 'sensu-plugins-monit/version'
-
-# Load the defaults
-
-#
-# Default class
-#
-module SensuPluginsMonit
-  class << self
-  end
-
-  class << self
-  end
-end
